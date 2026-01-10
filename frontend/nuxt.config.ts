c95e7fc1c52168341e2053bf2eae391bff7dd270
Kristo Baricevic <kristo.baricevic@gmail.com>
2026-01-09 22:02:13 -0500

    init frontend

---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || "http://localhost:8000/api",
+    },
+  },
+})
