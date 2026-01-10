60fbc41ff9ed97126eecf60adea098fb1a89b0b0
Kristo Baricevic <kristo.baricevic@gmail.com>
2026-01-09 23:35:37 -0500

    update css

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,9 +1,9 @@
 export default defineNuxtConfig({
   srcDir: "app",
+  css: ["~/assets/app.css"],
   runtimeConfig: {
     public: {
       apiBase: process.env.NUXT_PUBLIC_API_BASE || "http://localhost:8000/api",
     },
-    css: ["~/assets/app.css"],
   },
 })
